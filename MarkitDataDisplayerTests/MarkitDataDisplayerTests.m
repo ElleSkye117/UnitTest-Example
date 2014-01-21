de6e86370bcb03d30a7ffa8b5fa36440710b2036
Michael Leber <mikeleber117@gmail.com>
2014-01-21 16:05:11 -0700

    initial commit, set up pods

--- MarkitDataDisplayerTests/MarkitDataDisplayerTests.m
+++ MarkitDataDisplayerTests/MarkitDataDisplayerTests.m
@@ -6,29 +6,48 @@
 //  Copyright (c) 2014 Markit. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+#import <Specta/Specta.h>
+#define EXP_SHORTHAND
+#import <Expecta/Expecta.h>
 
-@interface MarkitDataDisplayerTests : XCTestCase
+@interface MarketSymbol : NSObject
+@property (nonatomic, copy) NSString *symbolName;
+@end
+@interface MarketDisplayController : UIViewController
+@property (nonatomic, weak) IBOutlet UILabel *symbolNameLabel;
+
+- (void)setSymbol:(MarketSymbol*)symbol;
 
 @end
 
-@implementation MarkitDataDisplayerTests
-
-- (void)setUp
+@implementation MarketDisplayController
+- (void)setSymbol:(MarketSymbol*)symbol
 {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    [self.symbolNameLabel setText:symbol.symbolName];
 }
 
 @end
+
+
+SpecBegin(TestSpec)
+
+// Descriptive name here self documents the code. People quickly know what it is testing and how it should behave
+it(@"On first load, the symbol label view, should contain the name of the symbol", ^{
+    // Set up in three parts
+    // Arrange
+    MarketDisplayController *controller = [[MarketDisplayController alloc] init];
+    MarketSymbol *stubSymbol = [[MarketSymbol alloc] init];
+    [stubSymbol setSymbolName:@".SYM"];
+    
+    [controller setSymbol:stubSymbol];
+    
+    // Act
+    // Loads the view, basically simulates how iOS View controller architecture will access this.
+    [controller view];
+    
+    // Assert
+    // There should only one "expect" (or "verify" if using a mock object). If we need to check more, then we can have more "it()" cases for each.
+    expect(controller.symbolNameLabel.text).to.equal(@".SYM");
+});
+
+SpecEnd
